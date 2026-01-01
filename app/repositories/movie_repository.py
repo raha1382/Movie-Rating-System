40266ce47d75bff511d5f596d0ba02ee69604946
raha1382 <143132177+raha1382@users.noreply.github.com>
2026-01-01 15:52:53 +0330

    feat: Sync with latest main

--- app/repositories/movie_repository.py
+++ app/repositories/movie_repository.py
@@ -1,8 +1,13 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 from app.models.movie import Movie
 from app.models.genre import Genre
+from app.models.rating import Rating
+from app.models.director import Director
+from sqlalchemy import func
 
 class MovieRepository:
+
+    # CRUD Methods
 
     def create(self, db: Session, movie: Movie) -> Movie:
         db.add(movie)
@@ -28,3 +33,42 @@ class MovieRepository:
     def delete(self, db: Session, movie: Movie) -> None:
         db.delete(movie)
         db.commit()
+
+    # Aggregation / Ratings Methods
+
+    def fetch_movies_with_aggregation(self, db: Session, skip: int = 0, limit: int = 10):
+        query = (
+            db.query(
+                Movie,
+                Director.name.label("director_name"),
+                func.coalesce(func.avg(Rating.score), 0).label("average_rating"),
+                func.count(Rating.id).label("ratings_count")
+            )
+            .join(Movie.director)
+            .outerjoin(Movie.ratings)
+            .group_by(Movie.id, Director.name)
+            .order_by(Movie.id)
+            .offset(skip) 
+            .limit(limit)
+        )
+        return query.all()
+
+    def fetch_movie_by_id(self, db: Session, movie_id: int):
+        movie = db.query(Movie)\
+                  .options(joinedload(Movie.genres), joinedload(Movie.director))\
+                  .filter(Movie.id == movie_id)\
+                  .first()
+        return movie
+
+    def fetch_movie_with_aggregation(self, db: Session, movie_id: int):
+        return db.query(
+            Movie,
+            Director.name.label("director_name"),
+            func.coalesce(func.avg(Rating.score), 0).label("average_rating"),
+            func.count(Rating.id).label("ratings_count")
+        ).join(Movie.director)\
+        .outerjoin(Movie.ratings)\
+        .filter(Movie.id == movie_id)\
+        .group_by(Movie.id, Director.name)\
+        .first()
+
