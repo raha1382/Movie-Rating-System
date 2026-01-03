d9f3567ab8382cd54e3516eba180247b64433dbb
FatemehKeshavarzi <fatemeh.kesha98@gmail.com>
2026-01-03 19:07:48 +0330

    refactor genric

--- app/api/schemas/movie.py
+++ app/api/schemas/movie.py
@@ -5,13 +5,13 @@ from datetime import datetime
 data_type = TypeVar("data_type")
 data_type2 = TypeVar("data_type2")
 
-class MovieCreateResponse[data_type](BaseModel):
+class MovieCreateResponse(BaseModel,  Generic[data_type]):
     status: Literal['success', 'failure'] = 'success'
     data: data_type
     updated_at: datetime | None = None
 
 
-class MovieOut[data_type2](BaseModel):
+class MovieOut(BaseModel, Generic[data_type2]):
     id: int
     title: str = Field(..., min_length=1)
     release_year: int = Field(..., ge=1888, le=2026)
